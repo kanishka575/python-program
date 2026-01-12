fdd18bf37170dea8d1fedbbde8e2f65da4bafc56
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 20:35:09 +0530

    Indexing

--- First program.py
+++ First program.py
@@ -20,16 +20,6 @@ b = int(input("enter a value:"))
 print(a>=b)
 
 
-#concatenation (addition of two strings are concatenation.)
-str1 = "hello"
-str2 = "world"
-final_str = str1+str2
-print("final_str")
-
-#length function 
-#for eg:- str = "hello"   len(str)
-# output will be 5
-
 #indexing / index function 
 str = "kanishka"
 print(str[3])
