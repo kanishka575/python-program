6b26089f718f46d018229ad1b389e4e1faeae40f
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 21:00:25 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,4 @@
+#this is slicing index concept. (while using this starting index is included but ending index is not included.)
+print(str[0:3])  # counting get start from 0 and ends at 3 index no. 
+print(str[:3])   # counting get start from 0 and ends at 3 index no. 
+print(str[0:])   # counting get start from 0 and ends at last letter of string. 
