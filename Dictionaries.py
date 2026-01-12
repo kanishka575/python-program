ef6b620c219ab246cb57a1d10d5736547dd5b03e
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 23:05:02 +0530

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,20 @@
+# dictionaries 
+#skeleton of dictionaries is like a.....
+dict = {
+    "key" : "value" 
+}
+print(dict)
+# for example :-
+info = {
+    "rohit" : "45",
+    "subject" : ["em","pp","cp"],
+    "roll no." : ("3","5","5","8","9"),
+    "cricket" : "cricketers"
+}
+print(info)
+print(type(info))
+
+# empty dict. 
+student = {}
+print(student)
+print(type(student))
