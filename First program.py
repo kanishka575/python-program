20c8d45c938870751bbb2763f498e598fc6c423d
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 22:59:15 +0530

    Tuples

--- First program.py
+++ First program.py
@@ -60,14 +60,6 @@ else:
     print("number is not multiple of 7")    
 
 
-# list methods. 
-list = [3,4,5,6]
-print(list.append(1))  # this method add the element to the list. 
-print(list.sort())     #this method sort the element in ascending order. 
-print(list.sort(reverse = True))   # this makes the list in reverse order. 
-print(list.reverse())
-print(list.insert("index","element"))
-
 #Tuples (immutable)
 tup = (1,2,3)
 print(tup[1])   # in tuples also we have index method. 
---
+++ tuples.py
@@ -0,0 +1,4 @@
+#Tuples (immutable)
+tup = (1,2,3)
+print(tup[1])   # in tuples also we have index method. 
+# single tuples must write in this way - (1,) while writting single element tuple comma is compulsory. 
