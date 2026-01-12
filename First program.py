6f952f509d4ffdbf3730b2812cf00f094794d51b
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 22:42:13 +0530

    list

--- First program.py
+++ First program.py
@@ -27,17 +27,7 @@ print("length of your name is",len(name))
 str = "i am a masterpiece of million $ & for selling for trillion $ " 
 print(str.count("$"))
            
-
-# nesting (loop inside the loop is basically the nesting )    
-age = 90
-
-if(age >= 18):
-    if(age >= 80):
-      print("cannot drive")
-    else:
-      print("can drive")
-else:
-    print("cannot drive")   
+ 
 
 # write a program to check if number entered is even or odd. 
 
---
+++ List.py
@@ -0,0 +1,9 @@
+# List
+marks = [45,64,99,1,9]   
+print(marks)
+print(type(marks))  # <class'list'>
+print(marks[0])
+print(marks[3])
+print(len(marks))
+# string in python are immutable. (which cannot get change)
+# list in python are mutable. (which can get change) , list is also having a slicing method.
