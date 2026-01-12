f88d3a09ad8672871f80d0741b2c650b34768ff3
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 20:08:31 +0530

    concatenation

---
+++ concatenation.py
@@ -0,0 +1,9 @@
+#concatenation (addition of two strings are concatenation.)
+str1 = "hello"
+str2 = "world"
+final_str = str1+str2
+print("final_str")
+
+#length function 
+#for eg:- str = "hello"   len(str)
+# output will be 5
