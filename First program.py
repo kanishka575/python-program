e6b4f541e76856a97938f51784eab1b95cd09f58
kanishka575 <kanishkaaa575@gmail.com>
2026-01-12 22:46:34 +0530

    listing methods clear

--- First program.py
+++ First program.py
@@ -60,16 +60,6 @@ else:
     print("number is not multiple of 7")    
 
 
- # List
-marks = [45,64,99,1,9]   
-print(marks)
-print(type(marks))  # <class'list'>
-print(marks[0])
-print(marks[3])
-print(len(marks))
-# string in python are immutable. (which cannot get change)
-# list in python are mutable. (which can get change) , list is also having a slicing method. 
-
 # list methods. 
 list = [3,4,5,6]
 print(list.append(1))  # this method add the element to the list. 
