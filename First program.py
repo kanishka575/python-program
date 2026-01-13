9767ab4c202297d5f1420e392461a3b6faddb45a
kanishka575 <kanishkaaa575@gmail.com>
2026-01-13 18:40:00 +0530

    sets

--- First program.py
+++ First program.py
@@ -90,24 +90,6 @@ else:
 grade = ("C","D","A","A","B","B","A")
 print(grade.count("A"))
 
-student =  {
-    "name" : "rohit",
-    "subject" : {
-        "phy" : "1",
-        "chem" : "2",     
-        "maths" :"3",
-    }
-}
-print(student)
-print(student["subject"])
-print(dict.keys())  # return all keys 
-print(dict.values())  # return all the values
-print(len(student))   # it tell us the no. of keys in it. 
-print(student.items()) # it return the (keys,values) as pair of tuples. 
-print(student.get("key")) # it return the key according to value. 
-student.update({"exam" : "19"}) # it add new dictionary into existing one. 
-print(student)
-
 
 # sets 
 set = {1,2,3,4,"45"}
---
+++ sets.py
@@ -0,0 +1,9 @@
+# sets 
+set = {1,2,3,4,"45"}
+print(set)
+print(type(set))
+print(len(set))  # total no. of items. 
+
+# empty set. 
+student = set() 
+print(student)
