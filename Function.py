d386d6dfcadc185cb045ef179ab69629fb8c20a1
kanishka575 <kanishkaaa575@gmail.com>
2026-01-24 12:44:41 +0530

    function

--- Function.py
+++ Function.py
@@ -5,3 +5,13 @@ def sum(a,b):  # parameters
     print("s = ",s)
     return s
 sum(4,5) # fuction call
+
+def prod(a = 4,b = 5):
+    print(a * b)
+    return a * b
+prod()  
+
+def prod(a,b = 5):  # Only non-default arguments can follow default argument.  
+    print(a * b)    # reverse is not possible
+    return a * b
+prod()
